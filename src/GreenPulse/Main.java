9bac863b18ff9ed2a6d7d289d11dd637b7abd38f
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-01 14:45:07 +0200

    Add the menu of the app in the Main class

--- src/GreenPulse/Main.java
+++ src/GreenPulse/Main.java
@@ -1,17 +1,53 @@
 package GreenPulse;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ManageUser userManager = new ManageUser();
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            System.out.println("choice one of those:");
+            System.out.print("    -> 1. Create new user:\n" +
+                            "    -> 2. Update user:\n" +
+                            "    -> 3. Delete user:\n" +
+                            "    -> 4. Display users:\n" +
+                            "    -> 5. Add consumption:\n" +
+                            "    -> 6. Display user consumption:\n" +
+                            "    -> 7. Exit\n");
+            System.out.print("enter your choice: ");
+            int choice = scanner.nextInt();
+            switch (choice){
+                case 1:
+                    userManager.createUser();
+                    break;
+                case 2:
+                    userManager.updateUser();
+                    break;
+                case 3:
+                    userManager.deleteUser();
+                    break;
+                case 4:
+                    userManager.displayUsers();
+                    break;
+                case 5:
+                    userManager.manageConsumption();
+                    break;
+                case 6:
+                    userManager.displayUserConsumptions();
+                    break;
+                case 7:
+                    scanner.close();
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("invalid choice");
+            }
         }
     }
+
+
 }
