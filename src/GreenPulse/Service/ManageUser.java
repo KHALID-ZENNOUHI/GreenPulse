1314d6b19cfcd48e1313380faea84614e215a5f6
Khalid Zennouhi <khalidzennouhi08@gmail.com>
2024-09-02 21:22:52 +0200

    move the manageConsumption and the displayUserConsumptions methods from the ManageUser class to a ManageCarbonConsumption calss to separate the user logic and the carbon consumption logic

--- src/GreenPulse/Service/ManageUser.java
+++ src/GreenPulse/Service/ManageUser.java
@@ -1,4 +1,7 @@
-package GreenPulse;
+package GreenPulse.Service;
+
+import GreenPulse.CarbonConsumption;
+import GreenPulse.User;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -9,7 +12,7 @@ import java.util.Scanner;
 public class ManageUser {
     private HashMap<String, User> users;
     private Scanner scanner;
-    private List<CarbonConsumption> consumptions;
+
 
     public ManageUser() {
         users = new HashMap<String, User>();
@@ -67,45 +70,9 @@ public class ManageUser {
         }
     }
 
-    public void manageConsumption(){
-        System.out.print("Enter the CIN of the user you want to add a consumption to: ");
-        String cin = scanner.next();
-        if (users.containsKey(cin)) {
-            User user = this.getUser(cin);
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-            System.out.print("enter the value of carbon consumption in kg: ");
-            Float carbonConsumption = this.scanner.nextFloat();
-            System.out.print("enter the start date:");
-            String startDate = this.scanner.next();
-            LocalDate startDateFormatted = LocalDate.parse(startDate, dateTimeFormatter);
-            System.out.print("enter the end date:");
-            String endDate = this.scanner.next();
-            LocalDate endDateFormatted = LocalDate.parse(endDate, dateTimeFormatter);
-            CarbonConsumption consumption = new CarbonConsumption(startDateFormatted, endDateFormatted, carbonConsumption, user);
-            user.addCarbonConsumption(consumption);
-        }else System.out.println("user does not exist!");
-    }
 
-    public void displayUserConsumptions(){
-        System.out.print("Enter the CIN of the user you want to add a consumption to: ");
-        String cin = scanner.next();
-        User user = users.get(cin);
-        if (user == null) {
-            System.out.println("User not found with CIN: " + cin);
-        }else {
-            List<CarbonConsumption> consumptions = user.getConsumptions();
 
-            if (consumptions == null || consumptions.isEmpty()) {
-                System.out.println("No consumptions found for user with CIN: " + cin);
-            } else {
-                System.out.println("name: " + user.getFirstName() + " " + user.getLastName());
-                System.out.println("age: " + user.getAge());
-                for (CarbonConsumption consumption : consumptions) {
-                    System.out.println("carbon consumption: " + consumption.getCarbon() + "kg" + " from: " + consumption.getStartDate() + " to: " + consumption.getEndDate());
-                }
-            }
-        }
-    }
+
 
     public User getUser(String cin){
         return users.get(cin);
